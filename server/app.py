90371c25393b142be9fec4f61a96ea036bd1fc85
Elchonon <ekn400@gmail.com>
2025-01-14 16:03:25 -0500

    lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,16 +28,33 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
-
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get_or_404(user_id)
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response('', 204)
 class Login(Resource):
-    pass
-
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(User.username == data['username']).first()
+        if user:
+            if user.authenticate(data['password']):
+                session['user_id'] = user.id
+                #print(user.id, session['user_id'])
+                return make_response(user.to_dict(), 200)
+        return make_response({"error":"Login Failed"}, 401)
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.clear()
+        return '', 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
